74b8d6d11ac04e787cf055837b12a938699fe8cf
Luana de Lima <luabranca.0987@gmail.com>
2025-05-26 17:01:44 -0300

    iniciando header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+import Logo from "../Logo/Logo"
+import "./Header.css"
+
+export default function Header(){
+  return(
+    <header>
+      <Logo src={'../../../Public/logo.svg'}
+      cor={"#C92071"}/>
+    </header>
+  )
+}

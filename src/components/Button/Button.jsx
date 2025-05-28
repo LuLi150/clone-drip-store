e4dbc538ff3081578baba55c9287ada0a149cfbe
Luana de Lima <luabranca.0987@gmail.com>
2025-05-28 16:55:14 -0300

    logo e useful

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,14 @@
 import './Button.css'
 
-export const Button = ({children, width, height, color}) => {
+export const Button = ({children, width, height, color, margin}) => {
   return(
    <button className='button'
    style={{
     width: width,
     height: height,
     color: color,
-    backgroundColor: color && '#f5f5f5'
+    backgroundColor: color && '#f5f5f5',
+    margin: margin
   }}
    >{children}</button>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,39 @@
 import './Header.css'
 import {Logo} from '../Logo/Logo'
 import { Button } from '../Button/Button';
+import { ShopCart } from '../ShopCart/ShopCart';
+import { Input } from '../Input/Input';
+import { Useful } from '../Useful/Useful';
+
 
 export const Header = () => {
   return (
    <header id="header">
       <div className='box-header'>
         <div className='box-header-children'>
-          <Logo margin={'.5rem 0 0.25rem'}/>
+          <Logo margin={'.5rem 0 0 0.25rem'}/>
+          <Input
+           type={'text'}
+           placeholder={'Pesquisar produto...'}
+           margin={'0 3rem 0 1.688rem'}
+           width={'34.938rem'}
+           height={'3.75rem'}
+           src={'../../../public/search.svg'}
+           />
+           <Useful
+            value={'Cadastre-se'}
+            margin={'1.2rem 0 0 0'}
+            color={'#474747'}
+            href={'#'}
+            textDecoration={'underline'}
+           />
         </div>
         <Button
         width={'7.125rem'}
         height={'2.5rem'}
-        color={'#C92071'}
+        margin={'2.75rem 0 0 1.875rem'}
         >Entrar</Button>
+        <ShopCart value={2}/>
       </div>
    </header>
   );

244b9b8df9c13d35d4b92525be1235542d198bf8
Luana de Lima <luabranca.0987@gmail.com>
2025-05-29 16:45:44 -0300

    final de header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import { Button } from '../Button/Button';
 import { ShopCart } from '../ShopCart/ShopCart';
 import { Input } from '../Input/Input';
 import { Useful } from '../Useful/Useful';
+import { Nav } from '../Nav/Nav';
 
 
 export const Header = () => {
@@ -27,6 +28,42 @@ export const Header = () => {
             href={'#'}
             textDecoration={'underline'}
            />
+           <Nav
+            position={'absolute'}
+            top={'8.375rem'}
+            gap={'2rem'}
+            width={'26.438rem'}
+            height={'1.813rem'}
+           >
+           <li>
+            <Useful 
+            value={'Início'}
+            color={'#666666'}
+            to={'/'}
+            />
+            </li>
+            <li>
+            <Useful 
+            value={'Produtos'}
+            color={'#666666'}
+            to={'/produtos'}
+            />
+            </li>
+            <li>
+            <Useful 
+            value={'Meus pedidos'}
+            color={'#666666'}
+            to={'/pedidos'}
+            />
+            </li>
+            <li>
+            <Useful 
+            value={'Ofertas'}
+            color={'#666666'}
+            to={'/ofertas'}
+            />
+            </li>
+          </Nav>
         </div>
         <Button
         width={'7.125rem'}
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import './Logo.css'
 
 export const Logo = ({color, margin}) => {
   return (
-      <a style={{ margin:margin}} className='logo' href="">
+      <Link style={{ margin:margin}} className='logo' href="to">
         <img src={!color ? "../../../public/logoPink.svg" 
           : "../../../public/logo-White.svg"}/>
         <span style={{color:color}}>Drip Store</span>
-      </a>
+      </Link>
   );
 }
